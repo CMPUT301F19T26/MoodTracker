dd688f87eb0c823503c80181d8bcd759ba181b81
Ankush Sharma <ankush1@ualberta.ca>
2019-11-07 16:02:52 -0700

    added a button from main activity

--- src/app/src/main/java/com/example/moodtracker/view/MainActivity.java
+++ src/app/src/main/java/com/example/moodtracker/view/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.moodtracker.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.moodtracker.R;
+import com.example.moodtracker.model.Mood;
+import com.example.moodtracker.view.mood.MoodHistoryActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button goto_history_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goto_history_list = findViewById(R.id.button);
+
+        goto_history_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MoodHistoryActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
+
 }
