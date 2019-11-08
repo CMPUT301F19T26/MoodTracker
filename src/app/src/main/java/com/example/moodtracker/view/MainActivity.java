322ea01f9d833f601a55763a6cf1a85d8a48242d
Sarthak Dhingra <sartdh@hotmail.com>
2019-11-07 18:36:09 -0700

    done preliminary google map API

--- src/app/src/main/java/com/example/moodtracker/view/MainActivity.java
+++ src/app/src/main/java/com/example/moodtracker/view/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.moodtracker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.moodtracker.R;
+import com.example.moodtracker.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+
+        User jared = new User("234");
+        intent.putExtra("USER", jared);
+        intent.putExtra("MODE", 1);
+
+        startActivity(intent);
     }
 }
