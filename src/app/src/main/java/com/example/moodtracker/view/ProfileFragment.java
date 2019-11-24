9d10f57ac9a2e34a67a3d520a9f47c53c5a0f297
abdulhafiz67 <abdulhafiz67@gmail.com>
2019-11-24 14:23:55 -0700

    t #

--- src/app/src/main/java/com/example/moodtracker/view/ProfileFragment.java
+++ src/app/src/main/java/com/example/moodtracker/view/ProfileFragment.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 //import com.example.moodtracker.view.AddMoodEvent;
 import com.example.moodtracker.R;
